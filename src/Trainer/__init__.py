6671142efcc704affc2369a0d6a1b87db66865c4
dino <nguyentuanqx2@gmail.com>
2024-10-15 22:50:38 +0700

    The local training process and global aggregation process

---
+++ src/Trainer/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is Trainer endpoint.
+@author
+- Van Tuan Nguyen (vantuan.nguyen@lqdtu.edu.vn)
+- Razvan Beuran (razvan@jaist.ac.jp)
+@create date 2023-12-11 00:28:29
+"""
+
+from .client_trainer import ClientTrainer
+from .global_aggregator import GlobalAggregator
